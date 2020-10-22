31e67e4d30faaa7a18eca83b39047c9636a4cbe0
Patrick Maltagliati <patmalt@gmail.com>
2020-10-22 08:53:03 -0500

    Initial Commit

---
+++ CombinePlayground/CombinePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePlaygroundApp.swift
+//  CombinePlayground
+//
+//  Created by Patrick Maltagliati on 10/22/20.
+//
+
+import SwiftUI
+
+@main
+struct CombinePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
